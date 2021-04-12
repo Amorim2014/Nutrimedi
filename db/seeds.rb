8808750076869191977c575eeb6c3cb69845cdb9
Use - Carlos Amorim <carlosalamorim@gmail.com>
2021-04-12 17:22:57 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #   
-Product.create(name: "test")
+Product.create(name: "test1")
